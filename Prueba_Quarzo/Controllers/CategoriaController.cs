faf7a65c6d31d551e95be9b5ad564648830df438
Elmer Salazar <elmer.salazar@uned.cr>
2021-09-28 12:30:50 -0600

    28 septiembre 12:30pm se termino todas las funcionalidades queda mejorar detalles de aspecto

--- Prueba_Quarzo/Controllers/CategoriaController.cs
+++ Prueba_Quarzo/Controllers/CategoriaController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Prueba_Quarzo.Models;
 
 namespace Prueba_Quarzo.Controllers
 {
@@ -11,6 +12,10 @@ namespace Prueba_Quarzo.Controllers
         // GET: Categoria
         public ActionResult Index()
         {
+            ViewBag.codigo = "";
+            ViewBag.nombre = "";
+            ViewBag.activo = "";
+            ViewBag.mensaje = "";
             return View();
         }
 
@@ -48,15 +53,47 @@ namespace Prueba_Quarzo.Controllers
             return View();
         }
 
-        // POST: Categoria/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        
+        [HttpGet]
+        public ActionResult Get_Modificar( Modelo_tabla_Categorias categoria)
         {
             try
             {
+                Operaciones_con_la_BD operacion = new Operaciones_con_la_BD();
+                Modelo_tabla_Categorias modelo = new Modelo_tabla_Categorias();
+                modelo.Codigo_Categoria = categoria.Codigo_Categoria;
+                modelo.Nombre = categoria.Nombre;
+                modelo.Activo = categoria.Activo;
+                List<Modelo_tabla_Categorias> list = new List<Modelo_tabla_Categorias>();
+
+                list = operacion.Modificar_Categoria(modelo);
+
+                List<string> codigo = new List<string>();
+                List<string> nombre = new List<string>();
+                List<string> activo = new List<string>();
+                
+
+                if (list.Count > 0 || list != null)
+                {
+                    foreach (var item in list)
+                    {
+                        codigo.Add(item.Codigo_Categoria + "");
+                        nombre.Add(item.Nombre + "");
+                        activo.Add(item.Activo + "");
+                        
+
+                    }
+
+                    ViewBag.codigo = codigo;
+                    ViewBag.nombre = nombre;
+                    ViewBag.activo = activo;
+                    ViewBag.mensaje = "Registro Modificado";
+
+                }
+
                 // TODO: Add update logic here
 
-                return RedirectToAction("Index");
+                return View("Index");
             }
             catch
             {
@@ -72,13 +109,28 @@ namespace Prueba_Quarzo.Controllers
 
         // POST: Categoria/Delete/5
         [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        public ActionResult Delete(Modelo_tabla_Categorias categoria)
         {
             try
             {
-                // TODO: Add delete logic here
+                ViewBag.codigo = "";
+                ViewBag.nombre = "";
+                ViewBag.activo = "";
 
-                return RedirectToAction("Index");
+                Operaciones_con_la_BD operacion = new Operaciones_con_la_BD();
+                Modelo_tabla_Categorias modelo = new Modelo_tabla_Categorias();
+                modelo.Codigo_Categoria = categoria.Codigo_Categoria;
+                modelo.Nombre = "Dato a borrar";
+                modelo.Activo = false;
+
+                string resultado ="";
+
+                resultado = operacion.Eliminar_Categoria(modelo);
+                ViewBag.mensaje = resultado;
+
+
+
+                return View("Index");
             }
             catch
             {
---
+++ Prueba_Quarzo/Controllers/Operaciones_con_la_BD.cs
@@ -0,0 +1,180 @@
+﻿/*
+En esta clase se establece los parámetros de conexión a la base datos, así como cada una de las consultas que se van realizar
+En esta clase es donde se hace la "magia"
+Esta agrupado por regiones para facilitar su lectura y comprensión
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Prueba_Quarzo.Models
+{
+    public class Operaciones_con_la_BD
+    {
+        //ruta de conexión a la base de datos 
+        //NOTA IMPORTANTE *******esta clave de conexion se debe ajustar al servidor local de la base de datos donde sea probado
+        private readonly string claveconexion = "Data Source=SILVER-365\\ELMERSERVER;Initial Catalog=BDCrudTest;Integrated Security = True;Current Language=Spanish; pooling=true;min pool size=5;max pool size=250;";
+
+        public string Claveconexion { get => claveconexion; }
+
+        #region consultar todos los productos de una categoría
+        public List<Modelo_tabla_Productos> Tabla_Productos(int idCategoria) //se envia consulta para obtner todos los productos
+        {
+            List<Modelo_tabla_Productos> lst = new List<Modelo_tabla_Productos>();
+            try
+            {
+                SqlConnection conectar;
+                SqlCommand orden = new SqlCommand();
+                conectar = new SqlConnection(Claveconexion); //le pasamos la ruta 
+                SqlDataReader leerDatos; // variable para lectura
+                                         //cadena coon el comnando para la base de datos
+                String comando_baseDatos = "exec Usp_Sel_Co_Produtos "+idCategoria+";";
+
+                //definir los parametros y ejecucion a la base datos
+                orden.CommandType = CommandType.Text;
+                orden.CommandText = comando_baseDatos;
+                orden.Connection = conectar;
+                conectar.Open();
+
+                leerDatos = orden.ExecuteReader(); // datos que devuelve la consulta en base datos
+
+
+                if (leerDatos.HasRows)
+                {
+                    while (leerDatos.Read()) //mientras haya lectura
+                    {
+                        lst.Add(new Modelo_tabla_Productos //llena la tablaCategoría con la lectura
+                        {
+                            Codigo_Producto = Convert.ToInt32(leerDatos.GetInt32(0)),
+                            Nombre = leerDatos.GetString(1),
+                            Precio = Convert.ToInt32(leerDatos.GetInt32(2)),
+                            Categoria = leerDatos.GetString(3)
+                        }); ;
+                    }
+                }
+                orden.Dispose();
+                conectar.Close();
+                return lst;
+            }
+            catch (Exception)
+            {
+
+                return lst;
+            }
+
+
+        }
+
+
+        #endregion
+
+        #region modificar una categoría de producto o insertar una nueva
+        public List<Modelo_tabla_Categorias> Modificar_Categoria(Modelo_tabla_Categorias nuevo_registro) //se modifica un Registro de Categoría o se inserta
+        {
+            
+            List<Modelo_tabla_Categorias> list = new List<Modelo_tabla_Categorias>();
+            try
+            {
+                //se envia el parametro a la base de datos
+                String parametro = "exec Usp_Ins_Co_Categoria @Codigo_Categoria, @Nombre, @Activo, 0;" ;
+                SqlConnection conectar;
+                SqlCommand orden = new SqlCommand();
+                conectar = new SqlConnection(Claveconexion);
+                
+                orden.CommandType = CommandType.Text;
+                orden.CommandText = parametro;
+                orden.Connection = conectar;
+                conectar.Open();
+
+                //parametrizacion de los datos hacia la base de datos
+                orden.Parameters.AddWithValue("@Codigo_Categoria", nuevo_registro.Codigo_Categoria);
+                orden.Parameters.AddWithValue("@Nombre", nuevo_registro.Nombre);
+                orden.Parameters.AddWithValue("@Activo", nuevo_registro.Activo);
+                SqlDataReader leerDatos; // variable para lectura
+                leerDatos = orden.ExecuteReader(); // datos que devuelve la consulta en base datos
+
+                    if (leerDatos.HasRows)
+                    {
+                        while (leerDatos.Read()) //mientras haya lectura
+                        {
+                            list.Add(new Modelo_tabla_Categorias //llena la tablaCategoría con la lectura
+                            {
+                                Codigo_Categoria = Convert.ToInt32(leerDatos.GetInt32(0)),
+                                Nombre = leerDatos.GetString(1),
+                                Activo = Convert.ToBoolean(leerDatos.GetBoolean(2)),
+                                
+                            }); ;
+                        }
+                    }
+
+                    orden.Dispose();
+                    conectar.Close();
+                    
+                
+
+            }
+            catch
+            {
+
+                
+            }
+
+            return list;
+        }
+
+        #endregion
+
+        #region eliminar una categoría
+        public string Eliminar_Categoria(Modelo_tabla_Categorias nuevo_registro) //se modifica un Registro de Categoría o se inserta
+        {
+            Boolean confirmacion;
+            string cadena = "No se puedo eliminar registro";
+            try
+            {
+                //se envia el parametro a la base de datos
+                String parametro = "exec Usp_Ins_Co_Categoria @Codigo_Categoria, @Nombre, @Activo, 1;";
+                SqlConnection conectar;
+                SqlCommand orden = new SqlCommand();
+                conectar = new SqlConnection(Claveconexion);
+               // SqlDataReader leerDatos; // variable para lectura
+
+                orden.CommandType = CommandType.Text;
+                orden.CommandText = parametro;
+                orden.Connection = conectar;
+                conectar.Open();
+
+                //parametrizacion de los datos hacia la base de datos
+                orden.Parameters.AddWithValue("@Codigo_Categoria", nuevo_registro.Codigo_Categoria);
+                orden.Parameters.AddWithValue("@Nombre", nuevo_registro.Nombre);
+                orden.Parameters.AddWithValue("@Activo", nuevo_registro.Activo);
+
+
+
+                confirmacion = orden.ExecuteNonQuery() > 0;
+                if (confirmacion)
+                {
+                    cadena = "Se elimino el registro";
+
+                    orden.Dispose();
+                    conectar.Close();
+                   
+                }
+
+            }
+            catch (Exception e)
+            {
+                cadena = "Paso un error " + e;
+
+            }
+
+            return cadena;
+        }
+        #endregion
+
+
+    }
+}
